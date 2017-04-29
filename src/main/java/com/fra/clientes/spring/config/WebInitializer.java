beaa5af50d51f052b1c1130b482f4ee8f8bc7d64
Renzo Ariel Felitti <renzo.ariel.felitti@accenture.com>
2017-04-29 15:26:51 -0300

    Java config implementadas. Configuracion por xml borrada completamente

--- src/main/java/com/fra/clientes/spring/config/WebInitializer.java
+++ src/main/java/com/fra/clientes/spring/config/WebInitializer.java
@@ -1,4 +1,4 @@
-package com.fra.clientes.config;
+package com.fra.clientes.spring.config;
 
 import java.io.FileNotFoundException;
 
@@ -8,7 +8,8 @@ import javax.servlet.ServletRegistration;
 
 import org.springframework.util.Log4jConfigurer;
 import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.support.XmlWebApplicationContext;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.util.Log4jConfigListener;
 
@@ -16,31 +17,17 @@ public class WebInitializer implements WebApplicationInitializer{
 
 	@Override
 	public void onStartup(ServletContext container) throws ServletException {
-		XmlWebApplicationContext appContext = new XmlWebApplicationContext();
-
-		String[] configLocations = { "/WEB-INF/dispatcher-servlet.xml", "/WEB-INF/applicationResources.xml" };
-		appContext.setConfigLocations(configLocations);
-
+		      		
+		// Create the 'root' Spring application context
+		AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
+		rootContext.register(MainConfig.class);
+		
 		container.addListener(Log4jConfigListener.class);
 		try {
 			Log4jConfigurer.initLogging("file:/apps/clientes-abm/log4j-clientes.properties", 300000);
 		} catch (FileNotFoundException e) {
-			System.out.println("Log4j properties file not found");
+			e.getStackTrace();
 		}
-				
-		ServletRegistration.Dynamic dispatcher = container.addServlet("dispatcher", new DispatcherServlet(appContext));
-		dispatcher.setLoadOnStartup(1);
-		dispatcher.addMapping("/");
-		
-		      		
-				/*		Proxima configuracion
-		// Create the 'root' Spring application context
-		AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
-		rootContext.register(MainConfiguration.class);
-		
-		container.setInitParameter("log4jConfiguration", "file:/apps/clientes-abm/log4j-clientes.properties");
-		container.addListener(Log4jConfigListener.class);
-		
 		
 		// Manage the lifecycle of the root application context
 		container.addListener(new ContextLoaderListener(rootContext));
@@ -50,10 +37,9 @@ public class WebInitializer implements WebApplicationInitializer{
 		dispatcherContext.register(DispatcherConfig.class);
 
 		// Register and map the dispatcher servlet
-		ServletRegistration.Dynamic dispatcher = container.addServlet(
-				"dispatcher", new DispatcherServlet(dispatcherContext));
+		ServletRegistration.Dynamic dispatcher = container.addServlet("dispatcher", new DispatcherServlet(dispatcherContext));
 		dispatcher.setLoadOnStartup(1);
 		dispatcher.addMapping("/");
-		*/
+		
 	}
 }
