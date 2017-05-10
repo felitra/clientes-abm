a390c5e91bab001ee6af028253c0246b9336547f
Renzo Ariel Felitti <renzo.ariel.felitti@accenture.com>
2017-05-10 13:39:51 -0300

    Junit test busqueda por id, alta y baja creados

--- src/test/java/com/fra/clientes/test/ClienteServiceTest.java
+++ src/test/java/com/fra/clientes/test/ClienteServiceTest.java
@@ -3,7 +3,6 @@
  */
 package com.fra.clientes.test;
 
-import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,7 +10,9 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.fra.clientes.models.Cliente;
 import com.fra.clientes.services.ClienteService;
@@ -29,38 +30,46 @@ import com.fra.clientes.spring.config.MainConfig;
 @ContextConfiguration(classes= {MainConfig.class})
 //TODO: Quitar configuracion web despues de arreglar el controller web
 @WebAppConfiguration
+@Transactional
+@TransactionConfiguration(defaultRollback=true, transactionManager="transactionManager")
 public class ClienteServiceTest {
 
 	@Autowired
 	ClienteService clienteService;
-	
 	Cliente cliente;
+	long idPrueba;
 	
 	@Before
 	public void setup(){
 		cliente = new Cliente("Renzo", "46025719", "Corvalan 2626", "Casa");
+		idPrueba = 1;
 	}
 	
 	@Test
-	//TODO: Investigar @Test + @Transactional y la posibilidad de usar BD en memoria
+	public void testGetClienteById() throws ServiceException{
+		Cliente c = clienteService.getClienteById(idPrueba);
+		Assert.assertNotNull(c);
+		System.out.println(String.format("Cliente obtenido por id correctamente."));
+	}
+	
+	@Test
 	public void testCrear() throws ServiceException {
 		clienteService.addCliente(cliente);
 		Assert.assertNotNull(cliente.getId());
 		System.out.println(String.format("%s creado con ID: %d.", cliente.toString(), cliente.getId()));
 	}
 	
-	@After
+	@Test
 	public void testDelete() throws ServiceException{
-		long id = cliente.getId();
-		clienteService.deleteClienteById(id);
+		clienteService.deleteClienteById(idPrueba);
 		
 		boolean clienteNotFound = false;
 		try {
-			clienteService.getClienteById(id);
+			clienteService.getClienteById(idPrueba);
 		} catch (ClienteNotFoundException e) {
 			clienteNotFound = true;
 		}
 		Assert.assertTrue(clienteNotFound);
-		System.out.println(String.format("Cliente con ID: %d borrado correctamente.", id));
+		System.out.println(String.format("Cliente borrado correctamente."));
 	}
 }
